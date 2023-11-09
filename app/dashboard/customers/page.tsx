fcfda3dd067fcf8949da46b35d03ab9793fae9c8
Harry Ngugi <120082371+zitaharry@users.noreply.github.com>
2023-11-09 12:29:19 +0300

    Add files via upload

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            Welcome to the Customers Page
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            Welcome to the Invoices Page
+        </div>
+    )
+}
